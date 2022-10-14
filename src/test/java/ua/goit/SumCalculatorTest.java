899bf222b48cbfe59e5a5b0aa7bfa4515ba0124a
Stas Kotelnykov <staskotelnykov@gmail.com>
2022-10-14 20:46:27 +0300

    add @BeforeEach

--- src/test/java/ua/goit/SumCalculatorTest.java
+++ src/test/java/ua/goit/SumCalculatorTest.java
@@ -1,23 +1,31 @@
 package ua.goit;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SumCalculatorTest {
+    private SumCalculator sumCalculator;
+
+    @BeforeEach
+    public void init(){
+        sumCalculator = new SumCalculator();
+    }
+
     @Test
     public void testThat1InputHandledCorrectly() {
-        int actual = new SumCalculator().sum(1);
+        int actual = sumCalculator.sum(1);
         Assertions.assertEquals(1, actual);
     }
 
     @Test
     public void testThat3InputHandledCorrectly() {
-        int actual = new SumCalculator().sum(3);
+        int actual = sumCalculator.sum(3);
         Assertions.assertEquals(6, actual);
     }
 
     @Test
     public void testThat0InputHandledCorrectly() {
-        Assertions.assertThrows(IllegalArgumentException.class, () -> new SumCalculator().sum(0));
+        Assertions.assertThrows(IllegalArgumentException.class, () -> sumCalculator.sum(0));
     }
 }
