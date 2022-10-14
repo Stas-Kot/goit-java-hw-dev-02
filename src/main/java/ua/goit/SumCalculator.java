e26791a135994f4e4ab68ae39d47b8ebdefcbe11
Stas Kotelnykov <staskotelnykov@gmail.com>
2022-10-14 20:23:24 +0300

    add SumCalculator class and tests

--- src/main/java/ua/goit/SumCalculator.java
+++ src/main/java/ua/goit/SumCalculator.java
@@ -1,2 +1,16 @@
-package ua.goit;public class SumCalculator {
+package ua.goit;
+
+public class SumCalculator {
+    public int sum(int n) {
+        if (n < 1) {
+            throw new IllegalArgumentException("Input should be > 0");
+        }
+        int res = 0;
+        int i = 0;
+        while (i <= n) {
+            res += i;
+            i++;
+        }
+        return res;
+    }
 }
--- src/test/java/ua/goit/SumCalculatorTest.java
+++ src/test/java/ua/goit/SumCalculatorTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package ua.goit;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class SumCalculatorTest {
-  
+    @Test
+    public void testThat1InputHandledCorrectly() {
+        int actual = new SumCalculator().sum(1);
+        Assertions.assertEquals(1, actual);
+    }
+
+    @Test
+    public void testThat3InputHandledCorrectly() {
+        int actual = new SumCalculator().sum(3);
+        Assertions.assertEquals(6, actual);
+    }
+
+    @Test
+    public void testThat0InputHandledCorrectly() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> new SumCalculator().sum(0));
+    }
 }
